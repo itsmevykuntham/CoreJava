12e54fbee0626403d5af7c02515923f8eb92274f
itsmevykuntham <itsmevykuntham@gmail.com>
2022-05-24 01:00:19 +0530

    comments in java

---
+++ Java/src/com/java/basics/Comments.java
@@ -0,0 +1,15 @@
+package com.java.basics;
+
+public class Comments {
+    public static void main(String[] args) {
+        System.out.println("// this is a single line comment");
+        System.out.println("/* this " +
+                "\nis " +
+                "\nmulti line comment " +
+                "\n*/");
+        System.out.println("/*" +
+                "\n*documentation" +
+                "\n*comment " +
+                "\n*/");
+    }
+}
