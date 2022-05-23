a7c48a4484943d809d12f1d8b52bd898f1cd49f9
itsmevykuntham <itsmevykuntham@gmail.com>
2022-05-24 01:03:32 +0530

    different data types

---
+++ Java/src/com/java/basics/DataTypes.java
@@ -0,0 +1,17 @@
+package com.java.basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int a = 111;
+        float b = 11.1f;
+        double c = 111.111;
+        char d = 'A';
+        boolean e = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
