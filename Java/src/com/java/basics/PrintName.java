19f74c55db4aa8afb29c0bb40d2f47e172e9bdec
itsmevykuntham <itsmevykuntham@gmail.com>
2022-05-24 00:55:49 +0530

    printing my name

---
+++ Java/src/com/java/basics/PrintName.java
@@ -0,0 +1,8 @@
+package com.java.basics;
+
+public class PrintName {
+    public static void main(String[] args) {
+        String name  = "Ravi Kumar Vykuntham";
+        System.out.println(name);
+    }
+}
