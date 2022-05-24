4d056d498c19155d1b741471f9548b7571d822d9
itsmevykuntham <itsmevykuntham@gmail.com>
2022-05-24 11:49:59 +0530

    multiple if else statements printing largest number

---
+++ Java/src/com/java/loops/MultipleIfElseStatement.java
@@ -0,0 +1,17 @@
+package com.java.loops;
+
+public class MultipleIfElseStatement {
+    public static void main(String[] args) {
+        int a = 10,b = 20,c = 30;
+
+        if(a>b && a>c){
+            System.out.println(a + " is the largest number");
+        }
+        else if(b>a && b>c){
+            System.out.println(b + " is the largest number");
+        }
+        else {
+            System.out.println(c + " is the largest number");
+        }
+    }
+}
