0ac44db606c0494efedd7547e62f7eb528367114
itsmevykuntham <itsmevykuntham@gmail.com>
2022-05-24 10:11:40 +0530

    print name 10 times using for loop

---
+++ Java/src/com/java/loops/PrintTenTimes.java
@@ -0,0 +1,9 @@
+package com.java.loops;
+
+public class PrintTenTimes {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println("Ravi Kumar Vykuntham " + i);
+        }
+    }
+}
